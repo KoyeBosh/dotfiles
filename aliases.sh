cf1c00d5b453755bb1007e0fcf15953643ace137
Koye <thekiingofmuffins@gmail.com>
2024-01-27 04:42:41 -0700

    Initial commit

---
+++ aliases.sh
@@ -0,0 +1,12 @@
+# Color output
+alias ls='exa --color=auto'
+alias grep='grep --color=auto'
+alias ip='ip -color=auto'
+
+# Shortcuts
+alias ll='ls -l'
+alias la='ll -a'
+alias c='clear'
+alias vim='nvim'
+alias svim='sudoedit'
+alias man='batman'
