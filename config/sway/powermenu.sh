ee135510200a2ebfa47a88a66e0508f7a8eab985
Koye <thekiingofmuffins@gmail.com>
2024-02-28 02:14:56 -0700

    Use config file for swayidle

--- config/sway/powermenu.sh
+++ config/sway/powermenu.sh
@@ -3,5 +3,5 @@
 case "$(echo -e "1 -  Shut down\n2 -  Reboot\n3 -  Log out" | wofi --sort-order=alphabetical --dmenu --insensitive --prompt "Power Menu" -H 190 -W 290)" in
 	"1 -  Shut down") exec systemctl poweroff;;
 	"2 -  Reboot") exec systemctl reboot;;
-	"3 -  Log out") exec hyprctl dispatch exit;;
+	"3 -  Log out") exec sway exit;;
 esac
