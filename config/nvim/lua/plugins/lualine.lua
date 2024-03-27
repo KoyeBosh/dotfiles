e67e5c13c975f23c96b6d089bc199e8acf601814
Koye <thekiingofmuffins@gmail.com>
2024-03-27 13:48:27 -0700

    Add those cool arrows backt to lualine

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -3,9 +3,9 @@ return {
 	dependencies = { 'nvim-tree/nvim-web-devicons' },
 	config = function()
 		require('lualine').setup({
-			options = {
-				section_separators = { left = '', right = '' }
-			}
+			-- options = {
+				-- section_separators = { left = '', right = '' }
+			-- }
 		})
 	end,
 }
